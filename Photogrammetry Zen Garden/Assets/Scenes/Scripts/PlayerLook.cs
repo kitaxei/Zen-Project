b9d58efd38b2fc7877dbd2677e35cd6d8e067536
Katie S <kunetida.singta@gmail.com>
2020-04-01 23:53:35 -0600

    Up and Down Locked Camera

--- Photogrammetry Zen Garden/Assets/Scenes/Scripts/PlayerLook.cs
+++ Photogrammetry Zen Garden/Assets/Scenes/Scripts/PlayerLook.cs
@@ -7,10 +7,12 @@ public class PlayerLook : MonoBehaviour
     [SerializeField] private string mouseXInputName, mouseYInputName;
     [SerializeField] private float mouseSensitivity;
 
+    private float xAxisClamp;
 
     private void Awake()
     {
         LockCursor();
+        xAxisClamp = 0.0f;
     }
 
     private void LockCursor()
@@ -28,7 +30,27 @@ public class PlayerLook : MonoBehaviour
         float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
 
+        xAxisClamp += mouseY;
+
+        if(xAxisClamp > 90.0f)
+        {
+            xAxisClamp = 90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(270.0f);
+        }
+        else if (xAxisClamp < -90.0f)
+        {
+            xAxisClamp = -90.0f;
+            mouseY = 0.0f;
+            ClampXAxisRotationToValue(90.0f);
+        }
         transform.Rotate(-transform.right * mouseY);
+    }
+    private void ClampXAxisRotationToValue(float value)
+    { 
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
     }
 }
 
