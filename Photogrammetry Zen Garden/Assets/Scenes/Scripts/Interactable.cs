1082972d2fca43501caa19e60a15b94921943c0b
Katie S <kunetida.singta@gmail.com>
2020-04-06 22:36:04 -0600

    changes

---
+++ Photogrammetry Zen Garden/Assets/Scenes/Scripts/Interactable.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Interactable : MonoBehaviour
+{
+
+    public bool isInRange;
+    public KeyCode interactKey;
+    public UnityEvent interactAction;
+    
+ void Start()
+    {
+
+    }
+
+void Update()
+    {
+        if(isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))
+            {
+                interactAction.Invoke(); //Fire Event 
+            }
+        }
+    }
+    private void OnTriggerEnter3D(Collider2D collision)
+    { if (collision.gameObject.CompareTag("Player"))
+        {
+            isInRange = true;
+            Debug.Log("Player now in range");
+        }
+    }
+
+    private void OnTriggerExit3D(Collider2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                isInRange = false;
+                Debug.Log("Player now in range");
+            }
+        }
+}
