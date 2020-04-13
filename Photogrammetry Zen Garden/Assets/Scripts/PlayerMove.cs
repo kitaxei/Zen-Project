2fb647228f0da47e10b726a6c2b16e84a103e70f
Katie S <kunetida.singta@gmail.com>
2020-04-12 22:12:44 -0600

    Added Lighting

---
+++ Photogrammetry Zen Garden/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] private string horizontalInputName;
+    [SerializeField] private string verticalInputName;
+    [SerializeField] private float movementSpeed;
+
+    private CharacterController charController;
+
+    void Start()
+    { }
+   
+    private void Awake()
+    {
+        charController = GetComponent<CharacterController>();
+        Cursor.visible = true;
+    }
+    private void Update()
+    {
+        PlayerMovement();
+    }
+    
+    private void PlayerMovement()
+    {
+        float horizInput = Input.GetAxis(horizontalInputName) * movementSpeed;
+        float vertInput = Input.GetAxis(verticalInputName) * movementSpeed;
+
+        Vector3 forwardMovement = transform.forward * vertInput;
+        Vector3 rightMovement = transform.right * horizInput;
+
+        charController.SimpleMove(forwardMovement + rightMovement);
+    }
+
+}
