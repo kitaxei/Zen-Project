f81b7eebc739aa45c833d198e006250e9e233c81
Katie S <kunetida.singta@gmail.com>
2020-04-12 18:31:26 -0600

    Text display animation

--- Photogrammetry Zen Garden/Assets/Scenes/Scripts/DialogueManager.cs
+++ Photogrammetry Zen Garden/Assets/Scenes/Scripts/DialogueManager.cs
@@ -42,7 +42,18 @@ public class DialogueManager : MonoBehaviour
             return;
         }
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
     }
 
     void EndDialogue()
