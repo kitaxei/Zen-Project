e97540aaa5b03089a8bfbaaa0568e489297135b8
Katie S <kunetida.singta@gmail.com>
2020-04-13 00:38:36 -0600

    Lighting Tests

--- Photogrammetry Zen Garden/Assets/Scripts/Lighting/LightingManager.cs
+++ Photogrammetry Zen Garden/Assets/Scripts/Lighting/LightingManager.cs
@@ -9,51 +9,36 @@ public class LightingManager : MonoBehaviour
     [SerializeField] private Light DirectionalLight;
     [SerializeField] private LightingPreset Preset;
     //Variables
-    [SerializeField Range(0, 24)] private float TimeOfDay;
+    [SerializeField, Range(0, 180)] private float TimeOfDay;
 
     private void Update()
     {
-        if (Preset == null)
-            return;
+        
 
         if(Application.isPlaying)
         {
             TimeOfDay += Time.deltaTime;
-            TimeOfDay %= 24; //Clamp between 0-24
-            UpdateLighting(TimeOfDay /24f)
+            TimeOfDay %= 180; //Clamp between 0-2
+            UpdateLighting(TimeOfDay / 180f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 180f);
         }
     }
 
     private void UpdateLighting(float timePercent)
     {
-        RenderSettings.ambientLight = Preset.Ambientcolor.Evaluate(timePercent);
-        RenderSettings.fogColor = Present.FogColor.Evaluate(timePercent);
+        RenderSettings.ambientLight = Preset.AmbientColor.Evaluate(timePercent);
+        RenderSettings.fogColor = Preset.FogColor.Evaluate(timePercent);
 
         if(DirectionalLight!=null)
         {
-            DirectionalLight.color = Present.DirectionalColor.Evaluate(timePercent);
+            DirectionalLight.color = Preset.DirectionalColor.Evaluate(timePercent);
             DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
         }
 
     }
 
-    private void OnValidate()
-    {
-        if (DirectionalLight != null)
-            return;
-
-        if (RenderSettings.sun != null)
-        {
-            DirectionalLight = RenderSettings.sun;
-        }
-
-        else
-        {
-            Light[] lights = GameObject.FindObjectsOfType<Light>();
-            foreach(LightingManager light in lights)
-            {
-
-            }
-        }
-    }
+   
 }
